af0ac35eda53e22a582040ea2c7b07095f4f6beb
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-03-10 13:29:21 +0200

    Add translateString

--- Polylang.php
+++ Polylang.php
@@ -183,6 +183,29 @@ class Polylang
                 return $language;
             },
         ]);
+
+        register_graphql_field('RootQuery', 'translateString', [
+            'type' => 'String',
+            'description' => __(
+                'Translate string using pll_translate_string() (Polylang)',
+                'wp-graphql-polylang'
+            ),
+            'args' => [
+                'string' => [
+                    'type' => [
+                        'non_null' => 'String',
+                    ],
+                ],
+                'language' => [
+                    'type' => [
+                        'non_null' => 'LanguageCodeEnum',
+                    ],
+                ],
+            ],
+            'resolve' => function ($source, $args, $context, $info) {
+                return pll_translate_string($args['string'], $args['language']);
+            },
+        ]);
     }
 
     function usesSlugBasedField(array $fields)
