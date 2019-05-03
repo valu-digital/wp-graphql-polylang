f87cd5fc7a6297ddf7064ae4602653a8d9a002aa
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-05-03 13:26:41 +0300

    First term tests

---
+++ tests/wpunit/TermObjectQueryTest.php
@@ -0,0 +1,56 @@
+<?php
+
+// XXX: Can we autoload this somehow?
+require_once __DIR__ . '/PolylangUnitTestCase.php';
+
+class TermObjectQueryTest extends PolylangUnitTestCase
+{
+    static function wpSetUpBeforeClass()
+    {
+        parent::wpSetUpBeforeClass();
+
+        self::set_default_language('en_US');
+        self::create_language('en_US');
+        self::create_language('fr_FR');
+        self::create_language('fi');
+        self::create_language('de_DE_formal');
+        self::create_language('es_ES');
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $term = wp_insert_term( 'entesttag', 'post_tag' );
+        pll_set_term_language($term['term_id'], 'en');
+
+        $term = wp_insert_term( 'fitesttag', 'post_tag' );
+        pll_set_term_language($term['term_id'], 'fi');
+    }
+
+    public function testListsTermsFromAllLanguages()
+    {
+        $query = '
+        query Tags {
+            tags {
+              nodes {
+                name
+              }
+            }
+         }
+        ';
+
+        $data = do_graphql_request($query);
+        $this->assertArrayNotHasKey('errors', $data, print_r($data, true));
+        $nodes = $data['data']['tags']['nodes'];
+        $expected = [
+            //
+            ['name' => 'entesttag'],
+            ['name' => 'fitesttag'],
+        ];
+
+        $this->assertEquals($nodes, $expected);
+    }
+
+
+}
