f6f29dcc213ee1d95a810679a97fb6918bf9614b
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-12-07 15:31:07 +0200

    hmm2

--- tools/add_languages.php
+++ tools/add_languages.php
@@ -1,5 +1,9 @@
 <?php
 
+# make sure PLL_Admin_Model is available
+if( ! defined( 'PLL_ADMIN' ) )    define( 'PLL_ADMIN',    true );
+if( ! defined( 'PLL_SETTINGS' ) ) define( 'PLL_SETTINGS', true );
+
 $polylang = PLL();
 
 function create_language($locale, $args = array())
