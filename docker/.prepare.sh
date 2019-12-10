91a26845bef8f0d8877d131cc0c5e5b0c8a018eb
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-12-10 14:32:23 +0200

    Upgrade valu/wp-testing-tools

--- docker/.prepare.sh
+++
@@ -1,7 +0,0 @@
-
-
-echo "# Generated file. Do not edit. Edit .env.docker instead" > .env
-echo "" >> .env
-cat .env.docker >> .env
-
-eval $(grep -v '^#' .env)
--- docker/init.sh
+++ docker/init.sh
@@ -7,20 +7,26 @@ if [ "${WP_DOCKER:-}" != "1" ]; then
     exit 1
 fi
 
+# Keep the container running so it can be accessed with the shell
+keepalive() {
+    exec tail -f /dev/null
+}
+
+if [ "${WPTT_NO_INIT:-}" = "1" ]; then
+    >&2 echo "Container running ok. Enter it with 'docker/run shell'"
+    keepalive
+fi
+
 composer install
 
 composer wp-install
 
-if [ -f init-docker.sh ]; then
-    # Run the custom init if any
-    exec ./init-docker.sh
-fi
 
 if [ "$(wp-install --status)" = "full" ]; then
-    >&2 echo "WP installed. You can access it from http://localhost:8080/ and run tests against it using ./docker/shell.sh"
+    >&2 echo "WP is running at http://localhost:8080/ and run tests against it using 'docker/run shell'"
     exec wp-install --serve
 else
     # Otherwise just keep the container running so it can be accessed with docker/shell.sh
-    >&2 echo "Init ok! Start the shell in antoher terminal with ./docker/shell.sh"
-    exec tail -f /dev/null
+    >&2 echo "Container running ok. Enter it with 'docker/run shell'"
+    keepalive
 fi
